3c62fcd9fd69cc32d713968ec9bc366d4581e8aa
Diego Miras Curras <diegomiras93@gmail.com>
2025-02-26 19:08:30 +0100

    Cambios de carpeta backend

--- backend/controller/controller.py
+++ backend/controller/controller.py
@@ -4,9 +4,9 @@ from backend.services.services_usuario import services_user
 from backend.services.services_facturas import services_factura
 from fastapi import APIRouter, UploadFile, File
 
-from model.modelos import Usuario
-from services.services_usuario import services_user
-import services.services_facturas.services_factura
+from backend.model.modelos import Usuario
+from backend.services.services_usuario import services_user
+import backend.services.services_facturas.services_factura
 
 router = APIRouter()
 
@@ -27,11 +27,11 @@ async def eliminar_usuario_id(id):
 
 @router.post("/procesar-factura")
 async def procesar_factura():
-    services.services_facturas.services_factura.procesar_factura()
+    backend.services.services_facturas.services_factura.procesar_factura()
 
 @router.post("/consulta-parseada")
 def instrucciones():
-    services.services_facturas.services_factura.consulta_parseada()
+    backend.services.services_facturas.services_factura.consulta_parseada()
 
 @router.post("/upload_factura")
 async def upload_factura(file:UploadFile = File(...)):
@@ -39,7 +39,7 @@ async def upload_factura(file:UploadFile = File(...)):
     content = await file.read()  # Leer el contenido del archivo
     file_size = len(content)  # Obtener el tamaño del archivo correctamente
 
-    response = await services.services_facturas.services_factura.serv_subir_factura(content, file.filename, file.content_type) # Lee el archivo y pasa el nombre
+    response = await backend.services.services_facturas.services_factura.serv_subir_factura(content, file.filename, file.content_type) # Lee el archivo y pasa el nombre
     return {
         "filename": file.filename,
         "content_type": file.content_type,
