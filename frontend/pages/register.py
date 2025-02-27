d294070bdd1a408678aacc3d6bae6c331f21bcb9
Julian Work <julian@hotmail.com>
2025-02-27 14:24:26 +0100

    frontend register almost done, with some bugs

---
+++ frontend/pages/register.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import sys
+import os
+import requests
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+from backend.controller.controller  import registro
+backend_url = "http://localhost:8000"
+
+def register():
+    st.title("Registrarse en FacturIA")
+ 
+
+
+    email = st.text_input("Correo electrónico")
+    password = st.text_input("Contraseña", type="password")
+   
+
+    user_data = {"email": email, "password": password}
+
+    if st.button("enviar"):
+       response_register = requests.post(f"{backend_url}/registro", json=user_data)
+
+       print(response_register)
+
+
+register()
---
+++ frontend/pages/test.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.title("¡Hola, Streamlit funciona!")
