0daec42eacfdaa1d711a472fbda6a9522fcedaad
Jose Manuel Lomba Goce <Jose.lomba.goce03@gmail.com>
2025-03-05 16:02:29 +0100

    Implementacion de logeo y pagina principal

--- frontend/index.py
+++ frontend/index.py
@@ -1,14 +1,22 @@
-from time import sleep
-from pages.login import login_page
 import streamlit as st
 
 # Configuración de la página
 st.set_page_config(page_title="FacturIA")
-st.title("Mainpage")
-# Sidebar
-with st.sidebar:
 
-    st.sidebar.success("Elije un apartado")
+# Verificar si el usuario ha iniciado sesión
+if "authenticated" not in st.session_state:
+    st.session_state.authenticated = False
 
-if __name__ == "__main__":
-    login_page()
+# Si el usuario está autenticado, mostrar la MainPage
+if st.session_state.authenticated:
+    pageManager = st.navigation([
+        st.Page("main.py", title="Principal", default=True)
+    ])
+else:
+    pageManager = st.navigation([
+        st.Page("home.py", title="Inicio", default=True),
+        st.Page("pages/login.py", title="Login"),
+        st.Page("pages/registro.py", title="Registro")
+    ])
+
+pageManager.run()
