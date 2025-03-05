e12290c97d8415b5d876121f73b4b2916b95bd38
Jose Manuel Lomba Goce <Jose.lomba.goce03@gmail.com>
2025-03-05 16:24:28 +0100

    Registro fix

--- frontend/pages/registro.py
+++ frontend/pages/registro.py
@@ -1,25 +1,69 @@
 import streamlit as st
 import sys
 import os
-import requests
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+import time
+import re
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from backend.controller.controller import registro
+from utils.throw_json_error import (
+    throw_json_error,
+)
 
-backend_url = "http://localhost:8000"
+
+def validar_email(email: str) -> bool:
+    regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return re.match(regex, email) is not None
 
 
-def register():
+def register_page():
+
     st.title("Registrarse en FacturIA")
 
     email = st.text_input("Correo electrónico")
     password = st.text_input("Contraseña", type="password")
+    confirm_password = st.text_input("Confirmar contraseña", type="password")
 
-    user_data = {"email": email, "password": password}
+    if st.button("Registrar"):
+        if not email or not password:
+            st.warning("Por favor, ingresa tu correo y contraseña.")
+        elif password != confirm_password:
+            st.warning("Las contraseñas no coinciden.")
+        elif not validar_email(email):
+            st.warning("Por favor, ingresa un correo electrónico válido.")
+        else:
+            user_data = {"email": email, "password": password}
 
-    if st.button("enviar"):
-        response_register = registro(user_data)
+            error_message = ""
+            status_code = 0
 
-        print(response_register)
+            try:
+                response_register = registro(user_data)
+
+                error_message = response_register["error"]
+                status_code = response_register["status_code"]
+
+                print("status_code:", status_code)
+                print("Mensaje del frontend:", error_message)
+
+            except Exception as e:
+                st.error("Hubo un error al procesar la respuesta.")
+
+            if status_code == 201:
+                st.success({error_message})
+                st.session_state["logged_in"] = True
+                st.session_state["email"] = email
+                st.write("Redirigiendo...")
+                time.sleep(3)
+                # st.rerun()
+
+            elif status_code == 400:
+                st.error(f"Error frontend: {error_message}")
+            elif status_code == 409:
+                st.error(f"Error frontend: {error_message}")
+            else:
+                st.error(f"Error desconocido frontend: {error_message}")
 
 
-register()
+register_page()
