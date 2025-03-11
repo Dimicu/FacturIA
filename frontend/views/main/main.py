51b47b34c8830053df3ded9150cd790e9e2f16e8
Jose Manuel Lomba Goce <Jose.lomba.goce03@gmail.com>
2025-03-11 18:22:55 +0100

    Añadir facturas y funciones de login

---
+++ frontend/views/main/main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from components.inicio.container_mis_facturas import misfacturasclass
+from components.inicio.container_economia import economiaclass
+from components.main.logout import logout
+
+# st.set_page_config(page_title="FacturIA", layout="wide")
+
+economiaclass().render_economia()
+misfacturasclass().misfacturas()
+logout().logoutbutton()
+
